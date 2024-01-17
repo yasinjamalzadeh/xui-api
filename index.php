2ed16543807e5bb8b0b207ca950132e8a757ac45
yasinjamalzadeh <jamalzadeh.yasin@gmail.com>
2024-01-17 16:00:37 +0330

    Added root path to the api

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <?php
 
-class v2Ray{
-    private $host;
-	private $port;
-    private $username;
-    private $password;
+class v2Ray
+{
+  private $host;
+  private $port;
+  private $username;
+  private $password;
+  private $rootPath;
 
-    function __construct($host, $port, $username, $password) {
-        $this->host = $host;
-		$this->port = $port;
-        $this->username = $username;
-        $this->password = $password;
-        if(!file_exists('./.cookie.txt')) $this->login();
-    }
+  function __construct($host, $port, $rootPath, $username, $password)
+  {
+    $this->host = $host;
+    $this->port = $port;
+    $this->rootPath = $rootPath;
+    $this->username = $username;
+    $this->password = $password;
+    if (!file_exists('./.cookie.txt'))
+      $this->login();
+  }
 
-    private function login(){
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://'.$this->host.':' . $this->port . '/login',
+  private function login()
+  {
+    $curl = curl_init();
+    curl_setopt_array(
+      $curl,
+      array(
+        CURLOPT_URL => 'http://' . $this->host . ':' . $this->port . '/' . $this->rootPath . '/login',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_COOKIEFILE => './.cookie.txt',
@@ -27,33 +35,39 @@ class v2Ray{
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_POSTFIELDS => 'password='.$this->password.'&username='.$this->username.'',
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
-    }
-    
-    private function guidv4($data = null) {
-        $data = $data ?? random_bytes(16);
-        assert(strlen($data) == 16);
-        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-    }
-    
-    private function RandomString($len=7){
-        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        $randstring = null;
-        for($i = 0; $i < $len; $i++)
-            $randstring .= $characters[rand(0, strlen($characters))];
-        return $randstring;
-    }
-    
-    public function users(){
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://'.$this->host.':' . $this->port . '/xui/inbound/list',
+        CURLOPT_POSTFIELDS => 'password=' . $this->password . '&username=' . $this->username . '',
+      )
+    );
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+  }
+
+  private function guidv4($data = null)
+  {
+    $data = $data ?? random_bytes(16);
+    assert(strlen($data) == 16);
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+  }
+
+  private function RandomString($len = 7)
+  {
+    $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    $randstring = null;
+    for ($i = 0; $i < $len; $i++)
+      $randstring .= $characters[rand(0, strlen($characters))];
+    return $randstring;
+  }
+
+  public function users()
+  {
+    $curl = curl_init();
+    curl_setopt_array(
+      $curl,
+      array(
+        CURLOPT_URL => 'http://' . $this->host . ':' . $this->port. '/' . $this->rootPath . '/xui/inbound/list',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -63,16 +77,20 @@ class v2Ray{
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        ));
-        $response = json_decode(curl_exec($curl), true);
-        curl_close($curl);
-        return $response['obj'];
-    }
-    
-    public function delete($uid){
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://'.$this->host.':' . $this->port . '/xui/inbound/del/' . $uid,
+      )
+    );
+    $response = json_decode(curl_exec($curl), true);
+    curl_close($curl);
+    return $response['obj'];
+  }
+
+  public function delete($uid)
+  {
+    $curl = curl_init();
+    curl_setopt_array(
+      $curl,
+      array(
+        CURLOPT_URL => 'http://' . $this->host . ':' . $this->port. '/' . $this->rootPath . '/xui/inbound/del/' . $uid,
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -82,28 +100,32 @@ class v2Ray{
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
-        ));
-        $response = json_decode(curl_exec($curl), true);
-        curl_close($curl);
-        return $response;
-    }
-    
-    public function genVmess($remark = null, $port = null, $traffic = 0){
-        if($remark == null) $remark = $this->RandomString();
-        if($port == null) $port = rand(11111, 99999);
-        $traffic = $traffic * 1024 * 1024 * 1024;
-        $guidv4 = $this->guidv4();
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://'.$this->host.':' . $this->port . '/xui/inbound/add'); 
-        curl_setopt($ch, CURLOPT_COOKIEFILE, './.cookie.txt');
-        curl_setopt($ch, CURLOPT_COOKIEJAR, './.cookie.txt');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'up=0&down=0&total='.$traffic.'&remark='.$remark.'&enable=true&expiryTime=0&listen=&port='.$port.'&protocol=vmess&settings={
+      )
+    );
+    $response = json_decode(curl_exec($curl), true);
+    curl_close($curl);
+    return $response;
+  }
+
+  public function genVmess($remark = null, $port = null, $traffic = 0)
+  {
+    if ($remark == null)
+      $remark = $this->RandomString();
+    if ($port == null)
+      $port = rand(11111, 99999);
+    $traffic = $traffic * 1024 * 1024 * 1024;
+    $guidv4 = $this->guidv4();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://' . $this->host . ':' . $this->port . '/' . $this->rootPath. '/xui/inbound/add');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, './.cookie.txt');
+    curl_setopt($ch, CURLOPT_COOKIEJAR, './.cookie.txt');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'up=0&down=0&total=' . $traffic . '&remark=' . $remark . '&enable=true&expiryTime=0&listen=&port=' . $port . '&protocol=vmess&settings={
   "clients": [
     {
-      "id": "'.$guidv4.'",
+      "id": "' . $guidv4 . '",
       "alterId": 0
     }
   ],
@@ -123,19 +145,19 @@ class v2Ray{
     "tls"
   ]
 }');
-        $response = curl_exec($ch);
-        curl_close ($ch);
-        usleep(150000);
-        $users = $this->users();
-        $myJson = [];
-        $myJson['type'] = 'vmess';
-        $myJson['id'] = $users[count($users) - 1]['id'];
-        $myJson['link'] = 'vmess://' . base64_encode('{
+    $response = curl_exec($ch);
+    curl_close($ch);
+    usleep(150000);
+    $users = $this->users();
+    $myJson = [];
+    $myJson['type'] = 'vmess';
+    $myJson['id'] = $users[count($users) - 1]['id'];
+    $myJson['link'] = 'vmess://' . base64_encode('{
   "v": "2",
-  "ps": "'.$remark.'",
-  "add": "'.$this->domain.'",
-  "port": '.$port.',
-  "id": "'.$guidv4.'",
+  "ps": "' . $remark . '",
+  "add": "' . $this->domain . '",
+  "port": ' . $port . ',
+  "id": "' . $guidv4 . '",
   "aid": 0,
   "net": "ws",
   "type": "none",
@@ -143,25 +165,28 @@ class v2Ray{
   "path": "/",
   "tls": "none"
 }');
-        return $myJson;
-    }
+    return $myJson;
+  }
 
-    public function genVless($remark = null, $port = null, $traffic = 0){
-        if($remark == null) $remark = $this->RandomString();
-        if($port == null) $port = rand(11111, 99999);
-        $traffic = $traffic * 1024 * 1024 * 1024;
-        $guidv4 = $this->guidv4();
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'http://'.$this->host.':' . $this->port . '/xui/inbound/add'); 
-        curl_setopt($ch, CURLOPT_COOKIEFILE, './.cookie.txt');
-        curl_setopt($ch, CURLOPT_COOKIEJAR, './.cookie.txt');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'up=0&down=0&total='.$traffic.'&remark='.$remark.'&enable=true&expiryTime=0&listen=&port='.$port.'&protocol=vless&settings={
+  public function genVless($remark = null, $port = null, $traffic = 0)
+  {
+    if ($remark == null)
+      $remark = $this->RandomString();
+    if ($port == null)
+      $port = rand(11111, 99999);
+    $traffic = $traffic * 1024 * 1024 * 1024;
+    $guidv4 = $this->guidv4();
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://' . $this->host . ':' . $this->port . '/' . $this->rootPath. '/xui/inbound/add');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, './.cookie.txt');
+    curl_setopt($ch, CURLOPT_COOKIEJAR, './.cookie.txt');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, 'up=0&down=0&total=' . $traffic . '&remark=' . $remark . '&enable=true&expiryTime=0&listen=&port=' . $port . '&protocol=vless&settings={
   "clients": [
     {
-      "id": "'.$guidv4.'",
+      "id": "' . $guidv4 . '",
       "flow": "xtls-rprx-direct"
     }
   ],
@@ -182,14 +207,14 @@ class v2Ray{
     "tls"
   ]
 }');
-        $response = curl_exec($ch);
-        curl_close ($ch);
-        usleep(150000);
-        $users = $this->users();
-        $myJson = [];
-        $myJson['type'] = 'vless';
-        $myJson['id'] = $users[count($users) - 1]['id'];
-        $myJson['link'] = 'vless://'.$guidv4.'@'.$this->host.':'.$port.'?type=ws&security=none&path=/#'.$remark;
-        return $myJson;
-    }
+    $response = curl_exec($ch);
+    curl_close($ch);
+    usleep(150000);
+    $users = $this->users();
+    $myJson = [];
+    $myJson['type'] = 'vless';
+    $myJson['id'] = $users[count($users) - 1]['id'];
+    $myJson['link'] = 'vless://' . $guidv4 . '@' . $this->host . ':' . $port . '?type=ws&security=none&path=/#' . $remark;
+    return $myJson;
+  }
 }
